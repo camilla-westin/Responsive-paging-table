50b8f8ef6ce3807195b4a6480cadd8cbb5e50f59
Camilla Westin <camilla@webstin.se>
2018-10-13 23:05:48 +0200

    Hide cells in mobile

--- scripts.js
+++ scripts.js
@@ -1,3 +1,22 @@
+var hiddenClass = "is-hidden";
+
 if ($(window).width() < 800) {
-   console.log('less than 800');
+   $('table').addClass('responsive-table');
+   initColumns();
+}
+
+function initColumns() {
+  $('tr').each((i, row) => {
+			let $row = $(row);
+			//Add .fixed-column to first child in row
+			$row.children().first().addClass('fixed-column');
+
+			//Hide all cells except for fixed column and second column
+			$row.children().not('.fixed-column').not(':nth-child(2)').addClass(hiddenClass);
+
+			//Add class with col + number to every cell in row
+			$row.children().slice(1).each((num, cell) => {
+				$(cell).addClass('col' + num);
+			});
+		});
 }
